17e83dd3ac4719c4fe8653a88498d91d359f1445
ncore-x <ncore.eng@gmail.com>
2025-09-05 23:40:00 +1100

    feat: move GET /hotels request inside repository

--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,6 +1,31 @@
+from sqlalchemy import func, select
 from models.hotels import HotelsOrm
 from repositories.base import BaseRepository
 
 
 class HotelsRepository(BaseRepository):
     model = HotelsOrm
+
+    async def get_all(
+        self,
+        location,
+        title,
+        limit,
+        offset,
+    ):
+        query = select(HotelsOrm)
+        if location:
+            query = query.filter(func.lower(
+                HotelsOrm.location).contains(location.strip().lower()))
+        if title:
+            query = query.filter(func.lower(
+                HotelsOrm.title).contains(title.strip().lower()))
+        query = (
+            query
+            .limit(limit)
+            .offset(offset)
+        )
+        result = await self.session.execute(query)
+
+        hotels = result.scalars().all()
+        return hotels
