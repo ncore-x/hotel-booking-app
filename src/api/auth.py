6894bfcc18e972a49e5f547f59b32c23aa8bb72b
ncore-x <ncore.eng@gmail.com>
2025-09-08 19:28:55 +1100

    feat: creating login api for auth and jwt token

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,5 +1,7 @@
+from datetime import datetime, timedelta, timezone
 from passlib.context import CryptContext
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
+import jwt
 
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserAdd, UserRequestAdd
@@ -9,6 +11,20 @@ router = APIRouter(prefix="/auth", tags=["Авторизация и аутент
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
+
+
+SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
+
+
+def create_access_token(data: dict) -> str:
+    to_encode = data.copy()
+    expire = datetime.now(timezone.utc) + \
+        timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    to_encode |= {"exp": expire}
+    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
+    return encoded_jwt
 
 
 @router.post("/register")
@@ -22,3 +38,16 @@ async def register_user(
         await session.commit()
 
     return {"status": "Ok"}
+
+
+@router.post("/login")
+async def login_user(
+    data: UserRequestAdd
+):
+    async with async_session_maker() as session:
+        user = await UsersRepository(session).get_one_or_none(email=data.email)
+        if not user:
+            raise HTTPException(
+                status_code=401, detail="Пользователь с таким email не зарегистрирован")
+        access_token = create_access_token({"user_id": user.id})
+        return {"access_token": access_token}
