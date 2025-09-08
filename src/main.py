04dff22764d19af38fda3def92eebacb22546a19
ncore-x <ncore.eng@gmail.com>
2025-09-08 18:21:04 +1100

    feat: add hashed password generation

--- src/main.py
+++ src/main.py
@@ -8,11 +8,13 @@ from pathlib import Path
 sys.path.append(str(Path(__file__).parent.parent))
 
 from src.api.hotels import router as router_hotels
+from src.api.auth import router as router_auth
 from src.config import settings
 
 
 app = FastAPI()
 
+app.include_router(router_auth)
 app.include_router(router_hotels)
 
 if __name__ == "__main__":
