a2cdb9f90085ec074c8502a916486082ba4c80b6
ncore-x <ncore.eng@gmail.com>
2025-09-08 18:35:56 +1100

    feat: restriction on creating the same email for a user

---
+++ src/migrations/versions/2025_09_08_1825-6068eeb7cdae_make_email_unique.py
@@ -0,0 +1,28 @@
+"""make email unique
+
+Revision ID: 6068eeb7cdae
+Revises: 99853efa4819
+Create Date: 2025-09-08 18:25:26.278543
+
+"""
+
+from typing import Sequence, Union
+
+from alembic import op
+import sqlalchemy as sa
+
+
+revision: str = "6068eeb7cdae"
+down_revision: Union[str, Sequence[str], None] = "99853efa4819"
+branch_labels: Union[str, Sequence[str], None] = None
+depends_on: Union[str, Sequence[str], None] = None
+
+
+def upgrade() -> None:
+    """Upgrade schema."""
+    op.create_unique_constraint(None, "users", ["email"])
+
+
+def downgrade() -> None:
+    """Downgrade schema."""
+    op.drop_constraint(None, "users", type_="unique")
