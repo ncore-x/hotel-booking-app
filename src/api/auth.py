cb5d2cd6d434c20ff83547c8635135a444730a7b
ncore-x <ncore.eng@gmail.com>
2025-09-09 17:47:59 +1100

    refactor: split authentication by files

--- src/api/auth.py
+++ src/api/auth.py
@@ -1,8 +1,6 @@
-from datetime import datetime, timedelta, timezone
-from passlib.context import CryptContext
 from fastapi import APIRouter, HTTPException, Response
-import jwt
 
+from src.services.auth import AuthService
 from src.repositories.users import UsersRepository
 from src.schemas.users import UserAdd, UserRequestAdd
 from src.database import async_session_maker
@@ -10,51 +8,30 @@ from src.database import async_session_maker
 router = APIRouter(prefix="/auth", tags=["Авторизация и аутентификация"])
 
 
-pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
-
-
-SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 30
-
-
-def verify_password(plain_password, hashed_password):
-    return pwd_context.verify(plain_password, hashed_password)
-
-
-def create_access_token(data: dict) -> str:
-    to_encode = data.copy()
-    expire = datetime.now(timezone.utc) + \
-        timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
-    to_encode |= {"exp": expire}
-    encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
-    return encoded_jwt
-
-
 @router.post("/register", summary="Регистрация пользователя")
 async def register_user(
-    data: UserRequestAdd
+        data: UserRequestAdd,
 ):
-    hashed_password = pwd_context.hash(data.password)
+    hashed_password = AuthService().hash_password(data.password)
     new_user_data = UserAdd(email=data.email, hashed_password=hashed_password)
     async with async_session_maker() as session:
         await UsersRepository(session).add(new_user_data)
         await session.commit()
+    return {"status": "OK"}
 
-    return {"status": "Ok"}
 
-
-@router.post("/login", summary="Аутентификация пользователя")
+@router.post("/login", summary="Авторизация пользователя")
 async def login_user(
-    data: UserRequestAdd
+        data: UserRequestAdd,
+        response: Response,
 ):
     async with async_session_maker() as session:
         user = await UsersRepository(session).get_user_with_hashed_password(email=data.email)
         if not user:
             raise HTTPException(
                 status_code=401, detail="Пользователь с таким email не зарегистрирован")
-        access_token = create_access_token({"user_id": user.id})
-        if not verify_password(data.password, user.hashed_password):
-            raise HTTPException(
-                status_code=401, detail="Пароль неверный")
+        if not AuthService().verify_password(data.password, user.hashed_password):
+            raise HTTPException(status_code=401, detail="Пароль неверный")
+        access_token = AuthService().create_access_token({"user_id": user.id})
+        response.set_cookie("access_token", access_token)
         return {"access_token": access_token}
--- src/config.py
+++ src/config.py
@@ -14,5 +14,9 @@ class Settings(BaseSettings):
 
     model_config = SettingsConfigDict(env_file=".env")
 
+    JWT_SECRET_KEY: str
+    JWT_ALGORITHM: str
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+
 
 settings = Settings()
