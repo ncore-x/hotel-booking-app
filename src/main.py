0d1f0d2b3032ba7cb4870c208a22019e6eb3384a
ncore-x <ncore.eng@gmail.com>
2025-09-12 18:07:13 +1100

    feat: creating routes, schemas, repositories for bookings

--- src/main.py
+++ src/main.py
@@ -10,6 +10,7 @@ sys.path.append(str(Path(__file__).parent.parent))
 from src.api.auth import router as router_auth
 from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
+from src.api.bookings import router as router_bookings
 
 
 app = FastAPI()
@@ -17,6 +18,7 @@ app = FastAPI()
 app.include_router(router_auth)
 app.include_router(router_hotels)
 app.include_router(router_rooms)
+app.include_router(router_bookings)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
