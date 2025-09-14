182b0374eb78a36a3a61ca4e421cf1e7b99adb3d
ncore-x <ncore.eng@gmail.com>
2025-09-14 20:55:50 +1100

    feat: creating a query to retrieve available rooms for booking: translating from SQL to ORM (completion)

--- src/repositories/rooms.py
+++ src/repositories/rooms.py
@@ -38,6 +38,7 @@ class RoomsRepository(BaseRepository):
             where date_from <= '2024-11-07' and date_to >= '2024-07-01'
             group by room_id
         """
+
         rooms_count = (
             select(BookingsOrm.room_id, func.count("*").label("rooms_booked"))
             .select_from(BookingsOrm)
@@ -49,13 +50,6 @@ class RoomsRepository(BaseRepository):
             .cte(name="rooms_count")
         )
 
-        """
-        rooms_left_table as (
-            select rooms.id as room_id, quantity - coalesce(rooms_booked, 0) as rooms_left
-            from rooms
-            left join rooms_count on rooms.id = rooms_count.room_id
-        )
-        """
         rooms_left_table = (
             select(
                 RoomsOrm.id.label("room_id"),
@@ -67,16 +61,23 @@ class RoomsRepository(BaseRepository):
             .cte(name="rooms_left_table")
         )
 
-        """
-        !select * from rooms_left_table
-        where rooms_left > 0
-        """
-
-        query = (
-            select(rooms_left_table)
-            .select_from(rooms_left_table)
-            .filter(rooms_left_table.c.rooms_left > 0)
+        rooms_ids_for_hotel = (
+            select(RoomsOrm.id)
+            .select_from(RoomsOrm)
+            .filter_by(hotel_id=hotel_id)
+            .subquery(name="rooms_ids_for_hotel")
         )
 
-        print(query.compile(bind=engine,
-              compile_kwargs={"literal_binds": True}))
+        rooms_ids_to_get = (
+            select(rooms_left_table.c.room_id)
+            .select_from(rooms_left_table)
+            .filter(
+                rooms_left_table.c.rooms_left > 0,
+                rooms_left_table.c.room_id.in_(rooms_ids_for_hotel)
+            )
+        )
+
+        # print(rooms_ids_to_get.compile(bind=engine,
+        #       compile_kwargs={"literal_binds": True}))
+
+        return await self.get_filtered(RoomsOrm.id.in_(rooms_ids_to_get))
