34ad0a06b629dc18361381b1e9866df1212e7548
ncore-x <ncore.eng@gmail.com>
2025-09-15 18:16:01 +1100

    feat: filtering and pagination to get hotels

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,4 +1,3 @@
-
 from datetime import date
 
 from fastapi import Query, APIRouter, Body
@@ -19,15 +18,14 @@ async def get_hotels(
         date_to: date = Query(example="2024-08-10"),
 ):
     per_page = pagination.per_page or 5
-    # return await db.hotels.get_all(
-    #     location=location,
-    #     title=title,
-    #     limit=per_page,
-    #     offset=per_page * (pagination.page - 1)
-    # )
+
     return await db.hotels.get_filtered_by_time(
         date_from=date_from,
         date_to=date_to,
+        location=location,
+        title=title,
+        limit=per_page,
+        offset=per_page * (pagination.page - 1)
     )
 
 
--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -13,14 +13,24 @@ class HotelsRepository(BaseRepository):
     model = HotelsOrm
     schema = Hotel
 
-    async def get_all(
-            self,
-            location,
-            title,
-            limit,
-            offset,
+    async def get_filtered_by_time(
+        self,
+        date_from: date,
+        date_to: date,
+        location=None,
+        title=None,
+        limit=None,
+        offset=None,
     ) -> list[Hotel]:
-        query = select(HotelsOrm)
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=date_from, date_to=date_to)
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+
+        query = select(HotelsOrm).filter(HotelsOrm.id.in_(hotels_ids_to_get))
         if location:
             query = query.filter(func.lower(
                 HotelsOrm.location).contains(location.strip().lower()))
@@ -32,21 +42,5 @@ class HotelsRepository(BaseRepository):
             .limit(limit)
             .offset(offset)
         )
-        print(query.compile(compile_kwargs={"literal_binds": True}))
         result = await self.session.execute(query)
-
         return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
-
-    async def get_filtered_by_time(
-            self,
-            date_from: date,
-            date_to: date,
-    ):
-        rooms_ids_to_get = rooms_ids_for_booking(
-            date_from=date_from, date_to=date_to)
-        hotels_ids_to_get = (
-            select(RoomsOrm.hotel_id)
-            .select_from(RoomsOrm)
-            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
-        )
-        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
