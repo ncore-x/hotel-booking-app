734df297e79783fad1ea480480e340d509ed4d36
ncore-x <ncore.eng@gmail.com>
2025-09-15 17:40:29 +1100

    feat: get available hotels

--- src/api/hotels.py
+++ src/api/hotels.py
@@ -1,24 +1,33 @@
+
+from datetime import date
+
 from fastapi import Query, APIRouter, Body
 
-from src.api.dependencies import DBDep, PaginationDep
-from src.schemas.hotels import HotelAdd, HotelPatch
+from src.api.dependencies import PaginationDep, DBDep
+from src.schemas.hotels import HotelPatch, HotelAdd
 
 router = APIRouter(prefix="/hotels", tags=["Отели"])
 
 
-@router.get("", summary="Получение отелей", description="Можно фильтровать по title или location")
+@router.get("", summary="Получение отелей")
 async def get_hotels(
-    pagination: PaginationDep,
-    db: DBDep,
-    location: str | None = Query(None, description="Локация"),
-    title: str | None = Query(None, description="Название отеля"),
+        pagination: PaginationDep,
+        db: DBDep,
+        location: str | None = Query(None, description="Локация"),
+        title: str | None = Query(None, description="Название отеля"),
+        date_from: date = Query(example="2024-08-01"),
+        date_to: date = Query(example="2024-08-10"),
 ):
     per_page = pagination.per_page or 5
-    return await db.hotels.get_all(
-        location=location,
-        title=title,
-        limit=per_page,
-        offset=per_page * (pagination.page - 1)
+    # return await db.hotels.get_all(
+    #     location=location,
+    #     title=title,
+    #     limit=per_page,
+    #     offset=per_page * (pagination.page - 1)
+    # )
+    return await db.hotels.get_filtered_by_time(
+        date_from=date_from,
+        date_to=date_to,
     )
 
 
--- src/repositories/hotels.py
+++ src/repositories/hotels.py
@@ -1,7 +1,11 @@
-from sqlalchemy import func, select
+from datetime import date
 
-from src.models.hotels import HotelsOrm
+from sqlalchemy import select, func
+
+from src.models.rooms import RoomsOrm
 from src.repositories.base import BaseRepository
+from src.models.hotels import HotelsOrm
+from src.repositories.utils import rooms_ids_for_booking
 from src.schemas.hotels import Hotel
 
 
@@ -10,11 +14,11 @@ class HotelsRepository(BaseRepository):
     schema = Hotel
 
     async def get_all(
-        self,
-        location,
-        title,
-        limit,
-        offset,
+            self,
+            location,
+            title,
+            limit,
+            offset,
     ) -> list[Hotel]:
         query = select(HotelsOrm)
         if location:
@@ -28,6 +32,21 @@ class HotelsRepository(BaseRepository):
             .limit(limit)
             .offset(offset)
         )
+        print(query.compile(compile_kwargs={"literal_binds": True}))
         result = await self.session.execute(query)
 
         return [Hotel.model_validate(hotel, from_attributes=True) for hotel in result.scalars().all()]
+
+    async def get_filtered_by_time(
+            self,
+            date_from: date,
+            date_to: date,
+    ):
+        rooms_ids_to_get = rooms_ids_for_booking(
+            date_from=date_from, date_to=date_to)
+        hotels_ids_to_get = (
+            select(RoomsOrm.hotel_id)
+            .select_from(RoomsOrm)
+            .filter(RoomsOrm.id.in_(rooms_ids_to_get))
+        )
+        return await self.get_filtered(HotelsOrm.id.in_(hotels_ids_to_get))
