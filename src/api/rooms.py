29368e3d1703d4ec6e0a41673fad0828c23d7b5a
ncore-x <ncore.eng@gmail.com>
2025-09-16 18:16:42 +1100

    feat: add relationship for selected room (facilities)

--- src/api/rooms.py
+++ src/api/rooms.py
@@ -25,7 +25,7 @@ async def get_room(
     room_id: int,
     db: DBDep
 ):
-    return await db.rooms.get_one_or_none(id=room_id, hotel_id=hotel_id)
+    return await db.rooms.get_one_or_none_with_rels(id=room_id, hotel_id=hotel_id)
 
 
 @router.post("/{hotel_id}/rooms", summary="Создание нового номера")
