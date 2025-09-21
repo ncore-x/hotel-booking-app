67779a97dbd284b49da0616e5823162169509306
ncore-x <ncore.eng@gmail.com>
2025-09-21 18:58:01 +1100

    feat: implementing the Pillow library and image compression

--- src/main.py
+++ src/main.py
@@ -16,6 +16,7 @@ from src.api.hotels import router as router_hotels
 from src.api.rooms import router as router_rooms
 from src.api.bookings import router as router_bookings
 from src.api.facilities import router as router_facilities
+from src.api.images import router as router_images
 
 
 @asynccontextmanager
@@ -33,6 +34,7 @@ app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_facilities)
 app.include_router(router_bookings)
+app.include_router(router_images)
 
 
 if __name__ == "__main__":
