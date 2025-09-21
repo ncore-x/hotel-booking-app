0d0606305fd47f957a516e6abbf9f19adeb9caa6
ncore-x <ncore.eng@gmail.com>
2025-09-21 18:13:52 +1100

    feat: installing Celery and creating tasks

--- src/api/facilities.py
+++ src/api/facilities.py
@@ -5,6 +5,7 @@ from fastapi_cache.decorator import cache
 from src.schemas.facilities import FacilityAdd
 from src.api.dependencies import DBDep
 from src.init import redis_manager
+from src.tasks.tasks import test_task
 
 
 router = APIRouter(
@@ -21,8 +22,8 @@ async def get_facilities(db: DBDep):
 async def create_facility(
     db: DBDep,
     facility_data: FacilityAdd = Body()
-
 ):
     facility = await db.facilities.add(facility_data)
     await db.commit()
+    test_task.delay()
     return {"status": "Ok", "data": facility}
