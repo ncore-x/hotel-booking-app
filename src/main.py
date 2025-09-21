727f230e3ac1de871f9f72d5b63942b203fde38d
ncore-x <ncore.eng@gmail.com>
2025-09-21 21:14:11 +1100

    test: remove example with fastapi infinite async task

--- src/main.py
+++ src/main.py
@@ -1,5 +1,4 @@
 # fmt: off
-import asyncio
 from contextlib import asynccontextmanager
 from fastapi import FastAPI
 import uvicorn
@@ -11,7 +10,6 @@ from fastapi_cache.backends.redis import RedisBackend
 
 sys.path.append(str(Path(__file__).parent.parent))
 
-from src.api.dependencies import get_db
 from src.init import redis_manager
 from src.api.auth import router as router_auth
 from src.api.hotels import router as router_hotels
@@ -21,21 +19,8 @@ from src.api.facilities import router as router_facilities
 from src.api.images import router as router_images
 
 
-async def send_emails_bookings_today_checkin():
-    async for db in get_db():
-        bookings = await db.bookings.get_bookings_with_today_checkin()
-        print(f"{bookings=}")
-
-
-async def run_send_email_regularly():
-    while True:
-        await send_emails_bookings_today_checkin()
-        await asyncio.sleep(5)
-
-
 @asynccontextmanager
 async def lifespan(app: FastAPI):
-    asyncio.create_task(run_send_email_regularly())
     await redis_manager.connect()
 
     FastAPICache.init(RedisBackend(redis_manager.redis), prefix="fastapi-cache")
@@ -49,7 +34,7 @@ app.include_router(router_hotels)
 app.include_router(router_rooms)
 app.include_router(router_facilities)
 app.include_router(router_bookings)
-app.include_router(router_images)
+app.include_router(router_images )
 
 
 if __name__ == "__main__":
