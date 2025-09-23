7740ad097e9a4ab223eafc23906530fa5786dfb5
ncore-x <ncore.eng@gmail.com>
2025-09-23 19:11:26 +1100

    test: creating a test database

--- tests/integration_tests/hotels/test_db.py
+++ tests/integration_tests/hotels/test_db.py
@@ -4,8 +4,8 @@ from src.database import async_session_maker
 
 
 async def test_add_hotel():
-    hotel_add = HotelAdd(title="Moscow Resort",
-                         location="Москва, ул. Московская 111")
+    hotel_data = HotelAdd(title="Moscow Resort",
+                          location="Москва, ул. Московская 111")
     async with DBManager(session_factory=async_session_maker) as db:
-        new_hotel_data = await db.hotels.add(hotel_add)
-        print(f"{new_hotel_data=}")
+        await db.hotels.add(hotel_data)
+        await db.commit()
