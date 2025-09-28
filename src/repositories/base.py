76b355e348092f5fecd7c4d8a0adcbc49927d1c2
ncore-x <ncore.eng@gmail.com>
2025-09-28 21:42:56 +1100

    chore: add exceptions for POST /booking (part 1)

--- src/repositories/base.py
+++ src/repositories/base.py
@@ -1,10 +1,12 @@
 from typing import Sequence, Any
 from sqlalchemy import select, insert, update, delete
 from pydantic import BaseModel
+from sqlalchemy.exc import NoResultFound
+from sqlalchemy.ext.asyncio import AsyncSession
 
+from src.exceptions import ObjectNotFoundException
 from src.database import Base
 from src.repositories.mappers.base import DataMapper
-from sqlalchemy.ext.asyncio import AsyncSession
 
 
 class BaseRepository:
@@ -31,15 +33,26 @@ class BaseRepository:
             return None
         return self.mapper.map_to_domain_entity(model)
 
+    async def get_one(self, **filter_by) -> BaseModel:
+        query = select(self.model).filter_by(**filter_by)
+        result = await self.session.execute(query)
+        try:
+            model = result.scalar_one()
+        except NoResultFound:
+            raise ObjectNotFoundException
+        return self.mapper.map_to_domain_entity(model)
+
     async def add(self, data: BaseModel) -> BaseModel | Any:
-        add_data_stmt = insert(self.model).values(**data.model_dump()).returning(self.model)
+        add_data_stmt = insert(self.model).values(
+            **data.model_dump()).returning(self.model)
         result = await self.session.execute(add_data_stmt)
         model = result.scalars().one()
         return self.mapper.map_to_domain_entity(model)
 
     async def add_bulk(self, data: Sequence[BaseModel]):
         add_data_stmt = (
-            insert(self.model).values([item.model_dump() for item in data]).returning(self.model)
+            insert(self.model).values([item.model_dump()
+                                       for item in data]).returning(self.model)
         )
         await self.session.execute(add_data_stmt)
 
